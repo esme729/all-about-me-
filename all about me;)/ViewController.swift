dc5b5794afb559961dc6225aa5c78a997e29b32e
Esmay <Molinaesme729@gmail.com>
2022-09-26 11:36:37 -0600

    Initial Commit

---
+++ all about me;)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  all about me;)
+//
+//  Created by Molina, Yadira - Student on 9/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
